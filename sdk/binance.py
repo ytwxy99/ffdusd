0671eed0341da4a43b186b4fe13f40baaee9887d
wangxiaoyu.tracy <wangxiaoyu.tracy@bytedance.com>
2025-02-07 20:54:57 +0800

    feat: 添加一些交易逻辑

--- sdk/binance.py
+++ sdk/binance.py
@@ -14,7 +14,6 @@ def fetch_current_price(exchange, symbol):
     try:
         ticker = exchange.fetch_ticker(symbol)
         last_price = ticker['last']
-        print(f"Current price of {symbol}: {last_price}")
         return last_price
     except Exception as e:
         print(f"Error fetching current price: {e}")
@@ -23,5 +22,57 @@ def fetch_current_price(exchange, symbol):
 
 # 查询symbol信息
 def fetch_symbol_market(exchange, symbol):
-    import pdb;pdb.set_trace()
     return exchange.fetch_market(symbol)
+
+
+# 查询所有委托单
+def fetch_open_orders(exchange, symbol):
+    try:
+        return exchange.fetch_open_orders(symbol)
+    except ccxt.BaseError as e:
+        print(f"Error fetching open orders: {e}")
+        return []
+
+
+# 撤销指定的订单
+def cancel_order(exchange, symbol, order_id):
+    try:
+        cancel_result = exchange.cancel_order(order_id, symbol)
+        print(f"Cancel order result: {cancel_result}")
+    except ccxt.BaseError as e:
+        print(f"Error canceling order: {e}")
+        return None
+
+
+# 现货买入
+def create_buy_limit_order(exchange, symbol, amount, price):
+    try:
+        order = exchange.create_limit_buy_order(symbol, amount, price)
+        order_id = order['id']  # 保存订单ID
+        print(f"Buy order created: {order}")
+        return order
+    except ccxt.BaseError as e:
+        print(f"Error creating buy order: {e}")
+        return None
+
+
+# 现货卖出
+def create_sell_limit_order(exchange, symbol, amount, price):
+    try:
+        order = exchange.create_limit_sell_order(symbol, amount, price)
+        order_id = order['id']  # 保存订单ID
+        print(f"Sell order created: {order}")
+        return order
+    except ccxt.BaseError as e:
+        print(f"Error creating sell order: {e}")
+        return None
+
+
+def check_order_status(order_id, symbol):
+    try:
+        order = exchange.fetch_order(order_id, symbol)
+        print(f"Order {order_id} is closed with status: {order['status']}")
+        return order
+    except ccxt.BaseError as e:
+        print(f"Error fetching order status: {e}")
+        return None
--- trade/strategy/fdusd.py
+++ trade/strategy/fdusd.py
@@ -1,4 +1,70 @@
+import time
 from sdk import binance
 
+T = {
+    "up": 0.0,
+    "low": 0.0,
+    "up_times": 0,
+    "low_times": 0,
+    "loop": False,
+}
+
 def do(exchange, symbol):
-    binance.fetch_current_price(exchange, symbol)
+    while True:
+        c_price = float(binance.fetch_current_price(exchange, symbol))
+        time.sleep(1)
+
+
+def decision_make(c_price):
+    global T
+    orders = binance.fetch_open_orders(exchange, symbol)
+
+    if T["up"] == 0.0 and T["low"] == 0.0:
+        T["up"] = c_price
+        T["low"] = c_price
+    
+    if c_price > T["up"]:
+        T["up"] = c_price
+        T["up_times"] = 0
+        T["loop"] = False
+
+    if c_price < T["low"]:
+        T["low"] = c_price
+        T["low_times"] = 0
+        T["loop"] = False
+
+    if c_price == T["up"] and c_price != T["low"]:
+        T["up_times"] = T["up_times"] + 1
+        print(f"Current price map of {symbol}: {T}")
+
+    if c_price == T["low"] and c_price != T["up"]:
+        T["low_times"] = T["low_times"] + 1
+        print(f"Current price map of {symbol}: {T}")
+
+
+    if T["up_times"] >= 3 and T["low_times"] >= 3:
+        # 如果已买入，则需要用"up" 加个挂单卖出
+        # return True  
+        if len(orders) == 0:
+            # 如果没有挂单，需要用"low" 价格挂单买入
+            binance.create_buy_limit_order(exchange, symbol, 6, T["up"])
+        else:
+            if T["loop"]:
+                return True
+
+            for order in orders:
+                od = order[info]
+                if not T["loop"] and od["side"] == "BUY":
+                    # 如果有买单且第一次触发这个条件时候，需要撤销重新用"low" 价格买入
+                    if not binance.cancel_order(exchange, symbol, od["orderId"]):
+                        T["loop"] = True
+                    
+                    binance.create_buy_limit_order(exchange, symbol, 6, T["low"])
+
+                elif not T["loop"] and od["side"] == "SELL":
+                    # 如果有卖单且第一次触发这个条件时候，需要撤销重新用"up" 价格卖出
+                    if not binance.cancel_order(exchange, symbol, od["orderId"]):
+                        T["loop"] = True
+                    
+                    binance.create_buy_limit_order(exchange, symbol, 6, T["up"])
+
