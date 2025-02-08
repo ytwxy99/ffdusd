9d1dc94c1285fce9d97fc23a913e87655d3ff489
wangxiaoyu.tracy <wangxiaoyu.tracy@bytedance.com>
2025-02-08 14:36:37 +0800

    feat: 完善db 操作函数

--- models/base.py
+++ models/base.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, Float, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -14,8 +14,22 @@ def InitDB(conf):
     # 创建基础模型
     Base = declarative_base()
     # 创建所有表
+    
+    class Market(Base):
+        __tablename__ = 'markets'
+
+        id = Column(Integer, primary_key=True)
+        side = Column(String, nullable=False) # buy/sell 交易方向
+        status = Column(String, nullable=False) # 订单状态
+        order_id = Column(String, nullable=False) # 订单id
+        price = Column(Float, nullable=False) #交易价格
+        deleted_at = Column(DateTime, nullable=True)
+
+
     Base.metadata.create_all(engine)
  
     # 创建会话
     Session = sessionmaker(bind=engine)
     session = Session()
+
+
---
+++ models/markets.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from models import base
+
+def create_market(session, market):
+    session.add(market)
+    session.commit()
+
+
+def fetch_market(session, order_id):
+    return session.query(base.Market).filter_by(order_id=order_id).first()
+
+
+def update_market_status(session, order_id, status):
+    market = session.query(base.Market).filter_by(order_id=order_id).first()
+    market.status = status
+    session.commit()
+    
+
+def delete_market(session, order_id):
+    to_delete = session.query(base.Market).filter_by(order_id=order_id).first()
+    if to_delete:
+        to_delete.deleted_at = datetime.utcnow()
+        session.commit()
